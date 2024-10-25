4650d0705acc2eea83233b8ef8541f975c73c1f2
phucanh08 <phucanhdn01@gmail.com>
2024-10-26 00:17:19 +0700

    feat: update new template

---
+++ docs/data.js
@@ -0,0 +1,12 @@
+const weddingData = {
+    groom: {
+        fullName: "Lê Phúc Anh",
+        shortName: "Phúc Anh",
+        birthday: "24/01/1998"
+    },
+    bride: {
+        fullName: "Nguyễn Thị Ngọc",
+        shortName: "Nguyễn Ngọc",
+        birthday: "28/12/1998"
+    }
+}
