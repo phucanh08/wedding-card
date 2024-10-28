42da96d79e300e43218725423797d37508b0bf56
phucanh08 <phucanhdn01@gmail.com>
2024-10-28 23:22:57 +0700

    feat: update firebase firestore

--- docs/firebase-config.js
+++ docs/firebase-config.js
@@ -24,7 +24,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
 const db = getFirestore(app);
 
 // Hàm thêm lời chúc
@@ -33,7 +33,7 @@ async function addWish(wish) {
         const time = serverTimestamp();
         const docRef = await addDoc(collection(db, "wishes"), {
             ...wish,
-            createTime: time
+            createdTime: time
         });
         console.log("Document written with ID: ", docRef.id);
         return docRef.id;
@@ -45,7 +45,11 @@ async function addWish(wish) {
 
 async function getAllWishes() {
     try {
-        const querySnapshot = await getDocs(collection(db, "wishes"));
+        const wishesQuery = query(
+            collection(db, "wishes"),
+            orderBy("createdTime", "desc"),
+        );
+        const querySnapshot = await getDocs(wishesQuery);
         const wishes = [];
         querySnapshot.forEach((doc) => {
             wishes.push({
@@ -62,7 +66,7 @@ async function getAllWishes() {
 }
 
 function setWishesListener(callback) {
-    const q = query(collection(db, "wishes"), orderBy("createTime", "desc"));
+    const q = query(collection(db, "wishes"), orderBy("createdTime", "desc"));
     return onSnapshot(q, (querySnapshot) => {
         const wishes = [];
         querySnapshot.forEach((doc) => {
@@ -73,7 +77,183 @@ function setWishesListener(callback) {
     });
 }
 
-export {
-    addWish,
-    getAllWishes
-};
+
+// Tự động gọi hàm khi trang load
+$(document).ready(function () {
+    setWishesListener(wishes => {
+        const comments = document.getElementById('show-comments');
+        comments.innerHTML = '';
+
+        wishes.forEach((wish, index) => {
+            const item = document.createElement('div');
+            item.className = 'box-comment pb-3';
+            item.innerHTML = '<div class="box-comment pb-3">\n' +
+                `                        <h4 id="user-name-comment" class="mt-1">${wish.name}</h4>\n` +
+                '                        <p id="comment-detail" class="m-0">\n' +
+                `                            ${wish.content}\n` +
+                '                        </p>\n' +
+                '                    </div>';
+            comments.appendChild(item);
+        })
+    });
+
+    /*------------------------------------------
+     = DONATE MODAL
+     -------------------------------------------*/
+    if ($("#donate-modal").length && $(".buttonDonate").length && $(".donate-modal-close").length) {
+        $(document).on('click', '.buttonDonate', function () {
+            $("#donate-modal").show();
+            if ($('body').hasClass('offcanvas')) {
+                $('body').removeClass('offcanvas');
+                $('.js-oliven-nav-toggle').removeClass('active');
+            }
+        });
+        $(document).on('click', '.donate-modal-close', function () {
+            $("#donate-modal").hide();
+        });
+        $(document).on('click', 'body', function (e) {
+            if (e.target.id == $("#donate-modal").attr('id')) {
+                $("#donate-modal").hide();
+            }
+        });
+    }
+
+    $(document).on('click', '.crypto-item', function () {
+        let parent = $(this).parents('.donate-card');
+        parent.find('.cryptos-box-view').show();
+        parent.find('.cryptos-box-view .coin-img').html('<img src="' + $(this).data('img') + '" />');
+        parent.find('.cryptos-box-view .coin-id').html($(this).data('id'));
+        parent.find('.cryptos-box-view .coin-address').html($(this).data('address'));
+        parent.find('.cryptos-box-view .coin-qr-code').html('').qrcode({
+            width: 150,
+            height: 150,
+            text: $(this).data('address')
+        });
+    });
+
+    $(document).on('click', '.cryptos-box-view-close', function () {
+        let parent = $(this).parents('.donate-card');
+        parent.find('.cryptos-box-view').hide();
+    });
+
+    /*------------------------------------------
+        = WISH FORM SUBMISSION
+    -------------------------------------------*/
+    if ($("#wish-form").length) {
+        $("#wish-form").validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 5
+                },
+                content: {
+                    required: true,
+                    minlength: 10
+                },
+                email: {
+                    required: false,
+                    email: true
+                },
+            },
+
+            errorPlacement: function (error, element) {
+                if (element.attr("name") === "content") {
+                    error.insertAfter("#wish-form .vitualTextarea");
+                } else {
+                    error.insertAfter(element);
+                }
+            },
+            messages: {
+                name: {
+                    required: '<span style="color:red;">Vui lòng nhập tên của bạn.</span>',
+                    minlength: '<span style="color:red;">Tên phải lớn hơn 5 ký tự.</span>',
+                },
+                content: {
+                    required: '<span style="color:red;">Vui lòng nhập lời chúc.</span>',
+                    minlength: '<span style="color:red;">Lời chúc phải lớn hơn 10 ký tự.</span>',
+                },
+                email: {
+                    email: '<span style="color:red;">Địa chỉ email không hợp lệ.</span>'
+                }
+            },
+
+            submitHandler: async function (form, event) {
+                try {
+                    event.preventDefault();
+                    const wish = {
+                        name: document.getElementById('name-comment').value,
+                        content: document.getElementById('detail-comment').value,
+                    };
+                    $("#loader").css("display", "inline-block");
+                    addWish(wish).then((result) => {
+                        $("#loader").hide();
+                        form.reset();
+                    });
+                } catch (e) {
+                    $("#loader").hide();
+                    $("#error").slideDown("slow");
+                    setTimeout(function () {
+                        $("#error").slideUp("slow");
+                    }, 5000);
+                }
+            }
+        });
+    }
+
+    /*------------------------------------------
+        = TOGGLE MUSUC BIX
+    -------------------------------------------*/
+    if ($(".music-box").length) {
+        var musicBtn = $(".music-box-toggle-btn"),
+            musicBox = $(".music-holder");
+
+        musicBtn.on("click", function () {
+            musicBox.toggleClass("toggle-music-box");
+            return false;
+        })
+    }
+
+    /*------------------------------------------
+        = BACK TO TOP
+    -------------------------------------------*/
+    if ($(".back-to-top-btn").length) {
+        $(".back-to-top-btn").on("click", function () {
+            $("html,body").animate({
+                scrollTop: 0
+            }, 2000, "easeInOutExpo");
+            return false;
+        })
+    }
+
+    $(document).on('click', '.calendar-button-custom-click', function (e) {
+        e.preventDefault();
+        $(this).parent().find('.calendar-button .atcb-click').click();
+    });
+
+    /*------------------------------------------
+    = MENU ACCESSBILITY
+    -------------------------------------------*/
+    $('.btn-menu-open').click(function () {
+        $('ul.list-menu-icon').css('opacity', '1');
+        $('ul.list-menu-icon').css('pointer-events', '');
+        $('.btn-menu-close').show();
+        $('.btn-menu-open').hide();
+    })
+    $('.btn-menu-close').click(function () {
+        $('ul.list-menu-icon').css('opacity', '0');
+        $('ul.list-menu-icon').css('pointer-events', 'none');
+        $('.btn-menu-open').show();
+        $('.btn-menu-close').hide();
+    })
+    setTimeout(() => {
+        $('.btn-menu-open').hide();
+        $('.btn-menu-close').show();
+        $('ul.list-menu-icon').css('opacity', '1');
+    }, 3000);
+    $(window).on("load", function () {
+        if ($('.bii-logo').length > 0) {
+            $('#menu-access').css('bottom', '278px');
+            document.querySelector('style').textContent += "@media (max-width: 799px){#menu-access{bottom: 238px!important;}}"
+        }
+    })
+});
