1d92c0b71aa458e115edddf52b553f16cf07556c
anhlp <phucanhdn08@gmail.com>
2026-01-16 20:11:37 +0700

    feat: Minify js and css

--- docs/wedding-data.js
+++ docs/wedding-data.js
@@ -1,12 +1,1 @@
-const weddingData = {
-    groom: {
-        fullName: "Lê Phúc Anh",
-        shortName: "Phúc Anh",
-        birthday: "24/01/1998"
-    },
-    bride: {
-        fullName: "Nguyễn Thị Ngọc",
-        shortName: "Nguyễn Ngọc",
-        birthday: "28/12/1998"
-    }
-}
+const weddingData={groom:{fullName:"Lê Phúc Anh",shortName:"Phúc Anh",birthday:"24/01/1998"},bride:{fullName:"Nguyễn Thị Ngọc",shortName:"Nguyễn Ngọc",birthday:"28/12/1998"}};
